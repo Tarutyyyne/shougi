13ea1814d2cc894affe7bfc6960ff2240cfcb464
Tarutyyyne <s1f102500405@iniad.org>
2025-05-08 21:19:07 +0900

    '今度こそ行けるのでは'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,99 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './page.module.css';
 
+const directions = [
+  [-1, 0], //上
+  [-1, 1], //右上
+  [0, 1], //右
+  [1, 1], //右下
+  [1, 0], //下
+  [1, -1], //左下
+  [0, -1], //左
+  [-1, -1], //左上
+];
+
+const pieceName: string[] = ['none', 'Pawn'];
+
+const showCandidateSite = () => {};
+
+//============以下home====================================
 export default function Home() {
+  const [turn, setTurn] = useState(1);
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  //以下structureClone()というboardの配列を変更する関数
+  const [lastCoordinate, setLastCoodinate] = useState([0, 0]);
+  const newBoard: number[][] = structuredClone(board);
+
+  const clickHandler = (
+    clickX: number,
+    clickY: number,
+    board: number[][],
+    directions: number[][],
+  ) => {
+    const candidateY: number = clickY - 1; //directions[0][0];
+    const candidateX: number = clickX + 0; //directions[0][1];
+    const lastY: number = clickY + 1;
+    const lastX: number = clickX - 0;
+    //空白を押したときは何もしない
+    console.log(clickY, clickX);
+    console.log(candidateY, candidateX);
+    if (board[clickY][clickX] === 0) {
+      console.log('stop function');
+      return;
+      //押したところが候補地だったら
+    } else if (board[clickY][clickX] === 2) {
+      console.log('before moving piece');
+      //歩の場合
+      if (board[lastY][lastX] === 1) {
+        console.log('move piece');
+        newBoard[clickY][clickX] = 1;
+        newBoard[lastY][lastX] = 0;
+        setBoard(newBoard);
+      }
+      //押したところが駒だったら
+    } else {
+      if (board[clickY][clickX] === 1) {
+        console.log('select piece');
+        console.log(clickY, clickX);
+        console.log(candidateY, candidateX);
+        newBoard[candidateY][candidateX] = 2;
+        setBoard(newBoard);
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            src/app/page.tsx
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board}>
+        {board.map((row, clickY) =>
+          row.map((color, clickX) => (
+            <div
+              className={styles.cell}
+              key={`${clickX}-${clickY}`}
+              onClick={() => clickHandler(clickX, clickY, board, newBoard)}
+            >
+              {color !== 0 && (
+                <div
+                  className={styles.piece}
+                  style={{ background: color === 1 ? '#000' : '#f00' }}
+                />
+              )}
+            </div>
+          )),
+        )}
+      </div>
     </div>
   );
 }
